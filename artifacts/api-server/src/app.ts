09fa7831ba9ae6e47fe7ad60bc0d72af596aa5a6
dasepsaeful-beep <dasepsaeful@hogy.co.id>
2026-05-21 11:43:01 +0700

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import { pinoHttp } from 'pino-http';
 import router from "./routes/index.js";
-import { logger } from "./lib/logger";
+import { logger } from "./lib/logger.js";
 
 // PERBAIKAN 1: Menggunakan express.Express atau langsung hapus anotasi tipenya karena sudah otomatis terdeteksi
 const app = express();
